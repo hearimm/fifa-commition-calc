14add16e5237b00ccb3c94f1d170692477f1bf10
Hulk Choi <doksul23@gmail.com>
2017-03-24 14:15:54 +0900

    대충 가닥 완성

--- fifa commition calc/ViewController.swift
+++ fifa commition calc/ViewController.swift
@@ -10,28 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var price: UITextField!
-    @IBOutlet weak var commition: UITextField!
-    @IBOutlet weak var discountRate: UITextField!
-    @IBOutlet weak var discountCommition: UITextField!
-    @IBOutlet weak var receivedAmount: UITextField!
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var commitionTxt: UITextField!
+    @IBOutlet weak var discountRateTxt: UITextField!
+    @IBOutlet weak var discountCommitionTxt: UITextField!
+    @IBOutlet weak var receivedAmountTxt: UITextField!
     
     @IBOutlet weak var unitSegment: UISegmentedControl!
     @IBOutlet weak var discountRateSegment: UISegmentedControl!
+    @IBOutlet var segmetCollection: [UISegmentedControl]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBAction func discountRateSegmentChanged(segment: UISegmentedControl) {
-        let discountRateArr = [0,0.3,0.4,0.5,0]
     
-        discountRate.value(forKey: discountRateArr[segment.selectedSegmentIndex].description)
+    //할인율 세그먼트 변경시 이벤트
+    @IBAction func discountRateSegmentChanged(_ segment: UISegmentedControl) {
+        
+        let discountRateArr = [0,30,40,50,0] //할인율 상수
+        let idx = segment.selectedSegmentIndex
+        let val = discountRateArr[idx].description //tostring
+        discountRateTxt.text = val //할인율 텍스트필드 입력
+        
+        if idx == 4 { //할인율 직접입력 시, enable
+            discountRateTxt.isEnabled = true
+        }else{
+            discountRateTxt.isEnabled = false
+        }
     }
     
     
-    @IBAction func priceValueChanged(_ sender: UITextField) {
+    
+    @IBAction func txtDoCalc(_ txt:UITextField){
+        doCalc()
     }
+    @IBAction func segmentDoCalc(_ segment: UISegmentedControl){
+        doCalc()
+    }
+    
+    func doCalc() {
+        if let priceString = priceTxt.text,
+            let price = Double(priceString),
+            let unit = getUnitVal(idx: unitSegment.selectedSegmentIndex),
+            let discountRateString = discountRateTxt.text ,
+            let discountRate = Double(discountRateString){
+                calc(price: price,unit: unit,discountRate: discountRate)
+        }
+        
+        
+    }
+    
+    func getUnitVal(idx : Int) -> Double? {
+        
+        switch idx {
+        case 0:
+            return 100000000
+        case 1:
+            
+            return 1000000
+        case 2:
+            return 10000
+        case 3:
+            return 1
+        default:
+            return nil
+        }
+    }
+    
+    func calc(price: Double, unit: Double , discountRate: Double) {
+        let priceVal = price * unit
+        var defaultCommition = 0.0
+        if priceVal < 1000 {
+            //alert('1000 ep이상 입력해주세요')
+        }
+        if priceVal > 500000 {
+            defaultCommition = 150000.0 + (priceVal - 500000.0) * 0.4
+        }else{
+            defaultCommition = priceVal * 0.3
+        }
+        
+        let discountCommition = defaultCommition * (discountRate / 100 )
+        let receviedAmount = priceVal - (defaultCommition - discountCommition)
+        
+        commitionTxt.text = Int(defaultCommition).description
+        discountCommitionTxt.text = Int(discountCommition).description
+        receivedAmountTxt.text = Int(receviedAmount).description
+        
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
